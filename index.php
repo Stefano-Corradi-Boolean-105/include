d1610c1f4c19bab94fb855e91a7124991313a47b
ugodeughi <stefano.corradi@gmail.com>
2023-10-27 13:12:16 +0200

    fine LC

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+// per non sbagliare mai le inclusioni usiamo sempre __DIR__ che resittuisce il percorso assoluto della cartella che contiene il file concatenato col perrcorso relativo del file da includere (preceduto dallo / )
+require_once __DIR__ . '/partials/template/header.php';
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Include</title>
+</head>
+<body>
+  <h1>Include</h1>
+  
+</body>
+</html>
---
+++ partials/template/header.php
@@ -0,0 +1,4 @@
+<?php
+
+require_once __DIR__ . '/../vars.php';
+require_once __DIR__ . '/../text.php';
